85407159a72c5b6b05211792ac2f674511657a1c
Vanik <vanik_888@mail.ru>
2014-03-27 00:32:48 +0400

    at least at most and so on with mocks

--- src/test/java/TestBank.java
+++ src/test/java/TestBank.java
@@ -16,14 +16,22 @@ public class TestBank {
     @BeforeMethod
     public void setUp() {
         System.out.println("Before method");
-        when(mockedBank.getAccount(10)).thenReturn(10);
-
     }
     @Test
     public void tesBank() {
         System.out.println("testMethod");
-        Assert.assertEquals(10, mockedBank.getAccount(10));
-        System.out.println(mockedBank.getAccount(100));
+        //.getAccount(10) was called 2 times
+        mockedBank.getAccount(10);
+        mockedBank.getAccount(10);
+        verify(mockedBank, times(2)).getAccount(10);
+
+        //getAccount(100) was called atLeast 1 time
+        mockedBank.getAccount(100);
+        mockedBank.getAccount(100);
+        mockedBank.getAccount(100);
+        verify(mockedBank, atLeast(1)).getAccount(100);
+        //getAccount(100) was called atMost 2 times
+        verify(mockedBank, atMost(2)).getAccount(100);
     }
     @AfterMethod
     public void tearDown() {
