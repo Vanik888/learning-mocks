27465487012364a3888e386b350b562856177be4
Vanik <vanik_888@mail.ru>
2014-03-27 00:23:45 +0400

    stubs when(bank.getAccount(id)).thenReturn(10);

--- src/test/java/TestBank.java
+++ src/test/java/TestBank.java
@@ -1,4 +1,7 @@
 import static org.mockito.Mockito.*;
+
+
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,13 +16,14 @@ public class TestBank {
     @BeforeMethod
     public void setUp() {
         System.out.println("Before method");
-//        this.bank = new Bank();
-        mockedBank.getAccount(100);
+        when(mockedBank.getAccount(10)).thenReturn(10);
+
     }
     @Test
     public void tesBank() {
         System.out.println("testMethod");
-        verify(mockedBank,times(1) ).getAccount(100);
+        Assert.assertEquals(10, mockedBank.getAccount(10));
+        System.out.println(mockedBank.getAccount(100));
     }
     @AfterMethod
     public void tearDown() {
